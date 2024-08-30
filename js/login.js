11ae3bb475c623a275ffd05f508ebd25cfa59559
vanessarinaldi <vane.rinaldif4@gmail.com>
2024-08-29 23:18:25 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('boton').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let user = document.getElementById('user').value;
+    let contra = document.getElementById('contra').value;
+
+    if (user && contra) {
+        location.replace('index.html');
+    } else {
+        alert('Please fill in the fields.');
+    }
+});
